4cd14893dc255a56a8a1a2201fb362f57c036055
Pankajaovhal <94462619+Pankajaovhal@users.noreply.github.com>
2021-11-26 10:52:53 +0530

    Delete q26 discount.py

--- q26 discount.py
+++
@@ -1,8 +0,0 @@
-quantity=int(input("pleawe enter total quantity:"))
-per_unit=int(input("please enter per unit:"))
-total_cost=quantity*per_unit
-discount=total_cost*10/100
-if total_cost>=1000:
-    print("you got discount","rs",discount,"thank you came again")    
-else:
-    print("you can't get discount")
