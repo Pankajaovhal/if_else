22d4e346f60b8d5443d7181d257749e58d08bf51
Pankajaovhal <94462619+Pankajaovhal@users.noreply.github.com>
2021-11-26 10:53:31 +0530

    Delete q6two maximum no.py

--- q6two maximum no.py
+++
@@ -1,8 +0,0 @@
-num1=int(input("please enter the number:"))
-num2=int(input("please enter the number:"))
-if num2<num1:
-    print(num1,"is a maximum number")
-elif num2>num1:
-    print(num2,"is a maximum number")
-else:
-    print("both are equal")
