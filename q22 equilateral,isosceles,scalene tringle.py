ebe63515e4367f988992b2568053a33fa94c3034
Pankajaovhal <94462619+Pankajaovhal@users.noreply.github.com>
2021-11-26 10:52:13 +0530

    Delete q22 equilateral,isosceles,scalene tringle.py

--- q22 equilateral,isosceles,scalene tringle.py
+++
@@ -1,9 +0,0 @@
-a=int(input("please enter value:"))
-b=int(input("please enter value:"))
-c=int(input("please enter value:"))
-if a==b and b==c:
-    print("equilateral triangle")
-elif a==b or b!=c:
-    print("isosceles angle")
-else:
-    print("scalene angle")
