9c6a612a8b5e6f4c98ee58d08213a9a76aa092c9
Pankajaovhal <94462619+Pankajaovhal@users.noreply.github.com>
2021-11-26 10:50:20 +0530

    Delete q14 alphabet,digit,special charcter.py

--- q14 alphabet,digit,special charcter.py
+++
@@ -1,9 +0,0 @@
-character=input("please input character:")
-if character>="a" and character<="z":
-    print(character,"is alphabet")
-elif character>="1" and character<="9":
-    print(character,"is number")
-elif character=="#" or character=="@" or character=="$":
-    print(character,"is a special character")
-else:
-    print("no")
