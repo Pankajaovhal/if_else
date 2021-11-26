4e407895701464d68a389fd7bf7c1c67bc569fac
Pankajaovhal <94462619+Pankajaovhal@users.noreply.github.com>
2021-11-26 10:52:02 +0530

    Delete q21 side of triangles.py

--- q21 side of triangles.py
+++
@@ -1,7 +0,0 @@
-a=int(input("please enter value:"))
-b=int(input("please enter value:"))
-c=int(input("please enter value:"))
-if a+b+c==180:
-    print(a+b+c,"valid sides of tringle")
-else:
-    print("invalid")
