ff0be0f3907f0b384c976c516fda3fbc47aa77ca
Pankajaovhal <94462619+Pankajaovhal@users.noreply.github.com>
2021-11-26 10:51:51 +0530

    Delete q20 angle of triangle.py

--- q20 angle of triangle.py
+++
@@ -1,7 +0,0 @@
-a=int(input("please enter the angle:"))
-b=int(input("please enter the angle:"))
-c=int(input("please enter the angle:"))
-if a+b+c==180:
-    print("valid")
-else:
-    print("not valid")
